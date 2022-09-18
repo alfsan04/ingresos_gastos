87da0da504b7554b2584546ed86a3ebd7de3bde5
Alfredo Sánchez Sánchez <alfsan04@gmail.com>
2022-09-18 19:32:26 +0200

    Con página de modificación y baja

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from registro_ig import app
+
 
 
 @app.route("/")
 def index():
-    return "Servidor rulando"
+    return render_template("index.html", pageTitle = "Lista")
+
+@app.route("/nuevo")
+def alta():
+    return render_template("new.html", pageTitle = "Alta")
+
+@app.route("/modification")
+def modificacion():
+    return render_template("modification.html", pageTitle = "Modificacion")
+
+@app.route("/baja")
+def baja():
+    return render_template("baja.html", pageTitle = "Baja")
