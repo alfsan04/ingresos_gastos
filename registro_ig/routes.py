33f666d39de34e2f5704841460a962c1e3240fd2
Alfredo Sánchez Sánchez <alfsan04@gmail.com>
2022-09-19 23:22:43 +0200

    Con la página de añadir gasto ya creada y funcionando

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,20 +1,35 @@
-from flask import render_template
+
+from flask import render_template, request, redirect
+import csv
 from registro_ig import app
-
-
 
 @app.route("/")
 def index():
-    return render_template("index.html", pageTitle = "Lista")
+    fichero = open("data/movimientos.txt", "r")
+    csvReader = csv.reader(fichero, delimiter=",", quotechar='"')
+    """movimientos = []
+    for movimiento in csvReader:
+        movimientos.append(movimiento)"""
+    movimientos = [movimiento for movimiento in csvReader] #esto es lo mismo que las tres líneas de arriba
 
-@app.route("/nuevo")
+    fichero.close()
+    return render_template("index.html", pageTitle = "Lista", movements=movimientos)
+
+@app.route("/nuevo", methods=["GET", "POST"])
 def alta():
-    return render_template("new.html", pageTitle = "Alta")
+    if request.method == "GET":
+        return render_template("new.html", pageTitle = "Alta")
+    else:
+        fichero = open("data/movimientos.txt", "a", newline = "")
+        csvWriter = csv.writer(fichero, delimiter=",", quotechar='"')
+        csvWriter.writerow([request.form["date"], request.form["concept"], request.form["quantity"]])
+        fichero.close()
+        return redirect("/")
 
 @app.route("/modification")
 def modificacion():
     return render_template("modification.html", pageTitle = "Modificacion")
 
-@app.route("/baja")
+@app.route("/delete")
 def baja():
-    return render_template("baja.html", pageTitle = "Baja")
+    return render_template("delete.html", pageTitle = "Baja")
